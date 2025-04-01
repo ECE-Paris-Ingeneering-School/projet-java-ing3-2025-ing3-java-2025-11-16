71001af71f09261c2444d1fe228591a3f1396c98
Elise Finel <elise.finel@edu.ece.fr>
2025-04-01 09:05:51 +0200

    rdvVUE

--- src/Vue/RendezVousVue.java
+++ src/Vue/RendezVousVue.java
@@ -1,4 +1,24 @@
 package Vue;
 
+import Modele.RendezVous;
+import java.util.List;
+
 public class RendezVousVue {
+
+    public void afficherRendezVous(RendezVous rdv){
+        System.out.println("Informations RendezVous : ");
+        System.out.println("ID : " + rdv.getIDrdv());
+        System.out.println("Patient: " + rdv.getPatient());
+        System.out.println("Spécialiste: " + rdv.getSpecialiste());
+        System.out.println("Date et Heure: " + rdv.dateHeure());
+        System.out.println("Statut: " + rdv.statut());
+    }
+
+    public void afficherListeRendezVous(List<RendezVous> rdvs){
+        System.out.println("Liste des Rendez-Vous :"){
+            for(RendezVous rdv : rdvs){
+                System.out.println(rdv);
+            }
+        }
+    }
 }
