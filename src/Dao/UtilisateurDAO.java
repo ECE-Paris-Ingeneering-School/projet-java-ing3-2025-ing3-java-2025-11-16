cff04caaa66b371bc5e92c4f4e5ca6eed9f1297a
Clara Mourot <clara.mourot2004@gmail.com>
2025-04-01 09:43:01 +0200

    Heritage classe patient et specialiste

--- src/Dao/UtilisateurDAO.java
+++ src/Dao/UtilisateurDAO.java
@@ -1,4 +1,14 @@
 package Dao;
 
-public class UtilisateurDAO {
+import Modele.Utilisateur;
+import java.util.List;
+
+public interface UtilisateurDAO {
+    List<Utilisateur> getAllUtilisateurs(); // Récupérer tous les utilisateurs
+
+    Utilisateur getUtilisateurById(int id); // Récupérer un utilisateur par son ID
+
+    void ajouterUtilisateur(Utilisateur utilisateur); // Ajouter un utilisateur
+
+    void supprimerUtilisateur(int id); // Supprimer un utilisateur
 }
