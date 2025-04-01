0b397939b77efba4e3bee39b4f9fa8bd666767bf
Elise Finel <elise.finel@edu.ece.fr>
2025-04-01 09:12:38 +0200

    SpecialisteVue

--- src/Vue/SpecialisteVue.java
+++ src/Vue/SpecialisteVue.java
@@ -1,4 +1,24 @@
 package Vue;
 
+import Modele.Specialiste;
+import java.util.List;
+
 public class SpecialisteVue {
+
+    public void afficherSpecialise(Specialiste specialiste){
+        System.out.println("Details du Spécialiste : ");
+        System.out.println("ID: "+ specialiste.getIDspecialiste());
+        System.out.println("Nom: "+ specialiste.getNom());
+        System.out.println("Prenom: "+ specialiste.getPrenom());
+        System.out.println("Specialisation: "+ specialiste.getSpecialisation());
+        System.out.println("Lieux de consultation: "+ specialiste.getLieuxConsultation());
+        System.out.println("Disponsibilités: "+ specialiste.getDisponibilites());
+    }
+
+    public void afficherListeSpecialistes(List<Specialiste> specialistes){
+        System.out.println("Liste des specialistes : ");
+        for(Specialiste specialiste : specialistes){
+            System.out.println(specialistes);
+        }
+    }
 }
