7c1179a74edb1fb0cafe3f5c18f57854137b4a12
Elise Finel <elise.finel@edu.ece.fr>
2025-04-01 09:31:41 +0200

    PatientVue

--- src/Vue/PatientVue.java
+++ src/Vue/PatientVue.java
@@ -1,23 +1,28 @@
 package Vue;
 
-import java.util.ArrayList;
+import Modele.Patient;
+import java.util.List;
 
 public class PatientVue {
-    private int IDpatient;
-    private String mdp;
-    private String nom;
-    private String prenom;
-    private String TypePatient;
-    private ArrayList<String> historiqueRDV;
-    private int inscriptionDate;
 
-    public PatientVue(int IDpatient, String mdp, String nom, String prenom, String typePatient, ArrayList historiqueRDV, int inscriptionDate) {
-        this.IDpatient = IDpatient;
-        this.mdp = mdp;
-        this.nom = nom;
-        this.prenom = prenom;
-        TypePatient = typePatient;
-        this.historiqueRDV = historiqueRDV;
-        this.inscriptionDate = inscriptionDate;
+    public void afficherPatient(Patient patient){
+        System.out.println("Details du Patient : ");
+        System.out.println("ID: " + patient.getIDpatient());
+        System.out.println("Nom: " + patient.getNom());
+        System.out.println("Prenom: " + patient.getPrenom());
+        System.out.println("Type de Patient : " + patient.getTypePatient());
+        System.out.println("Date d'inscription: " + patient.getInscriptionDate());
+        System.out.println("Historique des Rendez-Vous: " + patient.getHistoriqueRDV());
+    }
+
+    public void affihcerListePatients(List<Patient> patients){
+        System.out.println("Liste des Patients: ");
+        for(Patient patient : patients){
+            System.out.println("ID : " + patient.getIDpatient() +
+                    ", Nom : " + patient.getNom() +
+                    ", Prenom: " + patient.getPrenom() +
+                    ", Type: " + patient.getTypePatient());
+        }
+
     }
 }
